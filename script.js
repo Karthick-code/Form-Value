60e953fb0fa1bfcb780187922591e7223208694e
Karthick R <106647257+Karthick-code@users.noreply.github.com>
2024-05-13 20:31:07 +0530

    form details collecting using value

---
+++ script.js
@@ -0,0 +1,12 @@
+function details(){
+    var f_name=document.getElementById("Firstname").value;
+    console.log(f_name);
+    var m_name=document.getElementById("Middlename").value;
+    console.log(m_name);
+    var l_name=document.getElementById("Lastname").value;
+    console.log(l_name);
+    var email=document.getElementById("Email").value;
+    console.log(email);
+    var Location=document.getElementById("Location").value;
+    console.log(Location);
+}
